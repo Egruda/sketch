a8f9afe44fbe64e5db5f5a3d6c9dbc4dffb3b49f
Tanto <99785440+Egruda@users.noreply.github.com>
2023-05-23 16:25:01 +0700

    Create button to change #ofsquare. Use box-sizing.

--- script.js
+++ script.js
@@ -12,8 +12,48 @@ for(let i=0; i < 16; i++) {
         divChild[j] = document.createElement('div');
         divChild[j].classList.add(`c${j}`);
         divChild[j].classList.add('column');
-        divChild[j].innerText = 'Test';
         temp.appendChild(divChild[j]);
     }
+    const divColumn = document.querySelectorAll('.column');
+    divColumn.forEach(column => column.addEventListener('mouseenter', (e) => e.target.style.backgroundColor = 'black'));
 }
+
+
+
+const changeGridButton = document.querySelector('#changeGrid');
+changeGridButton.addEventListener('click', changeGrid);
+
+function changeGrid() {
+    
+    let dimension = 0;
+    do {
+    dimension = Number(prompt('Enter number of square per side up to 100: '));
+    } 
+    while (dimension > 100);
+
+    const divErase = document.querySelectorAll('.row');
+    divErase.forEach(erase => erase.remove());
+
+    for(let i=0; i < dimension; i++) {
+        divParent[i] = document.createElement('div');
+        divParent[i].classList.add(`r${i}`);
+        divParent[i].classList.add('row');
+        container.appendChild(divParent[i]);
+        temp = document.querySelector(`.r${i}`);
+        for(let j=0; j < dimension; j++) {
+            divChild[j] = document.createElement('div');
+            divChild[j].classList.add(`nc${j}`);
+            divChild[j].classList.add('ncolumn');
+            let size = 640/dimension;
+            divChild[j].style.width = size + 'px';
+            divChild[j].style.height = size + 'px';
+            temp.appendChild(divChild[j]);
+        }
+    }
+    const divnColumn = document.querySelectorAll('.ncolumn');
+    divnColumn.forEach(ncolumn => ncolumn.addEventListener('mouseenter', (e) => e.target.style.backgroundColor = 'black'));
+
+}
+
+
 
